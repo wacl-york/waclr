8c0b693c71b467f3e70b809a1e6a2f021dc02cc7
skgrange <s.k.grange@gmail.com>
2017-04-20 20:19:55 +0100

    add netcdf function and add ncdf4 as a depends

---
+++ R/tidy_faam_netcdf.R
@@ -0,0 +1,106 @@
+#' Function to transform a \code{NetCDF} file from FAAM activites to a data 
+#' frame.
+#' 
+#' @param file File names of FAAM \code{NetCDF} files. 
+#' 
+#' @author Stuart K. Grange
+#' 
+#' @return Data frame
+#' 
+#' @import dplyr
+#' 
+#' @examples 
+#' \dontrun{
+#' 
+#' # Create tidy data from a single file
+#' data_faam <- tidy_faam_netcdf(file = "data/core_faam_1hz.nc")
+#' 
+#' # Transform multiple files
+#' files <- c("data/core_faam_1hz.nc", "data/core_faam_10hz.nc")
+#' data_faam_many <- tidy_faam_netcdf(file = files)
+#' 
+#' }
+#' 
+#' @export
+tidy_faam_netcdf <- function(file) {
+  
+  data_frame(file = file) %>% 
+    rowwise() %>% 
+    do(tidy_faam_netcdf_worker(.$file)) %>% 
+    ungroup()
+  
+}
+
+
+tidy_faam_netcdf_worker <- function(file) {
+  
+  # Connect to file
+  ncdf <- ncdf4::nc_open(file)
+  
+  # Get variables in netcdf file
+  ncdf_variables <- names(ncdf$var)
+  
+  # Extract date from netcdf file, not within the variable structure
+  date_ncdf <- extract_date(ncdf)
+  
+  # Extract all variables in netcdf file
+  df <- plyr::llply(ncdf_variables, function(x) 
+    extract_variable(ncdf, x)) %>% 
+    do.call(cbind, .)
+  
+  # Add date to data frame and arrange variable order
+  df <- df %>% 
+    mutate(file = basename(file),
+           date = date_ncdf) %>% 
+    select(file,
+           date, 
+           starts_with("lat_", ignore.case = TRUE),
+           starts_with("lon_", ignore.case = TRUE),
+           starts_with("alt_", ignore.case = TRUE),
+           everything())
+  
+  return(df)
+  
+}
+
+
+
+# Define the functions
+extract_date <- function(ncdf, tz = "UTC") {
+  
+  # Get date piece
+  date <- ncdf$dim$Time
+  
+  # Parse date start
+  date_anchor <- date$units
+  date_anchor <- stringr::str_split_fixed(date_anchor, " ", 3)[, 3]
+  date_anchor <- lubridate::ymd_hms(date_anchor, tz = tz)
+  
+  # Get the date vector
+  date <- date$vals
+  
+  # Add vector to date anchor, just an epoch date
+  date <- date + date_anchor
+  
+  return(date)
+  
+}
+
+
+extract_variable <- function(ncdf, name) {
+  
+  # Get variable
+  value <- ncdf4::ncvar_get(ncdf, name)
+  
+  # Make a data frame
+  df <- data.frame(
+    value,
+    stringsAsFactors = FALSE
+  )
+  
+  # Give name
+  names(df) <- name
+  
+  return(df)
+  
+}
